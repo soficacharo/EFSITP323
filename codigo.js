1828ffda74daccad854ccd3e86cc98b95037c634
SOFIA CÁCHARO HERNÁEZ <47392186@ort>
2023-04-19 08:46:24 -0300

    había un error en la actualización de productos frente nuevas búsquedas. arreglado.

--- codigo.js
+++ codigo.js
@@ -8,15 +8,19 @@ fetch(`https://dummyjson.com/products/search?q=${busquedaUsuario}`)
     .then(res => {
         console.log(res)
         valores = document.getElementById("valores");
+        const resultados = document.getElementById("gridResultados");
+        resultados.innerHTML = null;
         res.products.forEach(actual => {
             const newElement = document.createElement("div");
             newElement.classList = "col"
             newElement.innerHTML = `<div class="card" style="width: 18rem;"><img src="${actual.images[0]}" class="card-img-top" alt="${actual.title}"><div class="card-body"><h5 class="card-title">${actual.title}</h5><p class="card-text">${actual.description}</p><a href="https://www.amazon.com/s?k=${actual.title}" class="btn btn-primary">Comprar por ${actual.price}$ </a></div></div>`
-            document.getElementById("gridResultados").appendChild(newElement) 
-        }
-        )
-    })
+            resultados.innerHTML += newElement.outerHTML;
+        })
+    }
+    )
     .catch(err => console.error("error", err))
 console.log("Fin consulta - fetch")
   }
 
+  const element = document.getElementById("resultadoRapidez");
+  element.innerHTML = `La tarea resuelta más rápidamente fue ${taskRapida}`
